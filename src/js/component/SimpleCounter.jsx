f05b18c19fa0018a9a9e57521bd6e11ec346aa50
JeanG1914 <jeanpaulgzea@gmail.com>
2023-01-27 00:34:56 +0000

    Counter running

--- src/js/component/SimpleCounter.jsx
+++ src/js/component/SimpleCounter.jsx
@@ -1,8 +1,39 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import ReactDom from "react-dom";
 function SimpleCounter (props){
+
     return (
+        <div className="bigCounter">
+            <div className="clock"><i class="fa-regular fa-clock"></i></div>
+            <div className="digitFour">{props.digitFour % 10}</div>
+            <div className="digitThree">{props.digitThree % 10}</div>
+            <div className="digitTwo">{props.digitTwo % 10}</div>
+            <div className="digitOne">{props.digitOne % 10}</div>
+        </div>
         
     )
 
 }
+SimpleCounter.propTypes ={
+    digitFour: PropTypes.number,
+    digitThree: PropTypes.number,
+    digitTwo: PropTypes.number,
+    digitFOne: PropTypes.number,
+}
+let counter = 0
+setInterval(function(){
+    const four = Math.floor(counter/1000)
+    const three = Math.floor(counter/100)
+    const two = Math.floor(counter/10)
+    const one = Math.floor(counter/1)
+    counter++;
+    ReactDom.render(
+        <SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four}/>,
+        document.querySelector("#app")
 
-export SimpleCounter
+    )
+    }
+,1000)
+
+export default SimpleCounter
--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,7 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import SimpleCounter from "./component/SimpleCounter";
+import SimpleCounter from "./component/SimpleCounter.jsx";
 
 //render your react application
 ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
